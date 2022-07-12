665ee643ba288381cf314528d7170851004c0ef1
takeru549 <tarutarusource121@gmail.com>
2022-07-12 11:20:33 +0000

    more-jsx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import React, {Compornent} from 'react';
 
 function App() {
+
+  const greeting = "Hi Takeru";
+  const dom = <h1 className='foo'>{greeting}</h1>;  
+  const domTextBox = <input type='text' onClick={() => {console.log('Why did you come to Japan?')} } />;
+  const returnDom = domTextBox;
   return (
-    <div>
-      <h1>Hello world!</h1>
-      <h2>Hello World222</h2>
-    </div>
+
+    <React.Fragment>
+      <label htmlFor='bar'>test</label> 
+      <input type='text' onClick={() => {console.log('Why did you come to Japan?')} } />
+    </React.Fragment>
+    // <div>
+    //   <h1>Hello world!</h1>
+    //   <h2>Hello World222</h2>
+    // </div>
   );
 }
 export default App;
