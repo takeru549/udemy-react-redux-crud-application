a59b3f56d731e20add293919f404c59ce0e039d1
takeru549 <tarutarusource121@gmail.com>
2022-07-12 11:49:31 +0000

    component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import React, {Compornent} from 'react';
+import React from 'react';
+
+// function App() {
+
+//   const greeting = "Hi Takeru";
+//   const dom = <h1 className='foo'>{greeting}</h1>;  
+//   const domTextBox = <input type='text' onClick={() => {console.log('Why did you come to Japan?')} } />;
+//   const returnDom = domTextBox;
+//   return (
+
+//     <React.Fragment>
+//       <label htmlFor='bar'>test</label> 
+//       <input type='text' onClick={() => {console.log('Why did you come to Japan?')} } />
+//     </React.Fragment>
+//     // <div>
+//     //   <h1>Hello world!</h1>
+//     //   <h2>Hello World222</h2>
+//     // </div>
+//   );
+//}
 
 function App() {
+  return(
+  <div>
+    <Dog />
+    <Dog />
+    <Dog />
+    <Dog />
+  </div>
+  )
 
-  const greeting = "Hi Takeru";
-  const dom = <h1 className='foo'>{greeting}</h1>;  
-  const domTextBox = <input type='text' onClick={() => {console.log('Why did you come to Japan?')} } />;
-  const returnDom = domTextBox;
-  return (
+}
 
-    <React.Fragment>
-      <label htmlFor='bar'>test</label> 
-      <input type='text' onClick={() => {console.log('Why did you come to Japan?')} } />
-    </React.Fragment>
-    // <div>
-    //   <h1>Hello world!</h1>
-    //   <h2>Hello World222</h2>
-    // </div>
-  );
+//関数コンポーネント
+const Dog = () => {
+  return <div>bowbow</div>;
 }
 export default App;
