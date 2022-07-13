f248268c1a9652dbbf8b7dceebc485c4055bfae9
takeru549 <tarutarusource121@gmail.com>
2022-07-13 02:00:01 +0000

    prop-types

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
 import React from 'react';
-
-// function App() {
-
-//   const greeting = "Hi Takeru";
-//   const dom = <h1 className='foo'>{greeting}</h1>;  
-//   const domTextBox = <input type='text' onClick={() => {console.log('Why did you come to Japan?')} } />;
-//   const returnDom = domTextBox;
-//   return (
-
-//     <React.Fragment>
-//       <label htmlFor='bar'>test</label> 
-//       <input type='text' onClick={() => {console.log('Why did you come to Japan?')} } />
-//     </React.Fragment>
-//     // <div>
-//     //   <h1>Hello world!</h1>
-//     //   <h2>Hello World222</h2>
-//     // </div>
-//   );
-//}
+import PropTypes from 'prop-types';
 
 function App() {
   const profiles = [
     {name: "Taro",age:10},
     {name: "Takeru", age:20},
-    {name: "ken"}
+    {name: "ken", age: 22}
   ]
   return(
   <div>
@@ -39,8 +21,9 @@ function App() {
 const User = (props) => {
   return <div>Hi Iam {props.name} and {props.age} years old</div>;
 }
-
-User.defaultProps = {
-  age :1
+User.prototypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired
 }
+
 export default App;
